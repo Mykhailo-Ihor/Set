ade12bdd4aaca5cc7ee0fbdba8ecc9a8bd74f8a3
Михайло-Ігор Проць <MykhailoIhor.Prots@lnu.edu.ua>
2024-03-27 11:27:57 +0200

    Додано реалізацію

--- Set/Set.h
+++ Set/Set.h
@@ -14,58 +14,89 @@ private:
 	unsigned size;
 public:
     Set() : head() {}
-
     Set(T val, Node* next = nullptr) { head = new Node(val, next); }
+    Set(const Set& other);
+    Set(T* elems, int n);
+    Set(T x);
+    ~Set();
+   
+    Set& add(T x);
+    Set& add(T* x, int size);
 
-    Set(const Set& other) : head(nullptr) 
+    Set union(const Set& T) const;
+    Set interset() const;
+    Set differ() const;
+    Set sym_diff() const;
+
+    bool Contain(const T& x) const;
+    Set& remove(const T& x); // if no such element - exception 
+    int size() const { return size; };
+    // Оператор []
+    T& operator[] (int index); 
+    // Константний оператор []
+    const T& operator[] (int index) const; 
+    // Оператор =
+    Set& operator=(const Set& other);
+    // Метод друку
+    void printAll() const; 
+
+};
+
+template<typename T>
+inline T& Set<T>::operator[](int index)
+{
+    Node* curr = head;
+    for (int i = 0; i < index && curr != nullptr; ++i)
     {
-        if (other.head == nullptr) 
+        curr = curr->next;
+    }
+    if (curr == nullptr)
+    {
+        std::ostringstream oss;
+        oss << "Error: Cannot get element at index #" << index << std::endl;
+        throw std::out_of_range(oss.str());
+    }
+    return curr->value;
+}
+template<typename T>
+inline const T& Set<T>::operator[](int index) const
+{
+    Node* curr = head;
+    for (int i = 0; i < index && curr != nullptr; ++i)
+    {
+        curr = curr->next;
+    }
+    if (curr == nullptr)
+    {
+        std::ostringstream oss;
+        oss << "Error: Cannot get element at index #" << index << std::endl;
+        throw std::out_of_range(oss.str());
+    }
+    return curr->value;
+}
+template<typename T>
+inline void Set<T>::printAll() const
+{
+    Node* curr = head;
+    if (curr == nullptr)
+    {
+        std::cout << "List is empty :(\n";
+    }
+    else
+    {
+        while (curr != nullptr)
         {
-            return;
-        }
-        head = new Node(other.head->value);
-        Node* curr = head;
-        Node* otherCurr = other.head->next;
-        while (otherCurr != nullptr) {
-            curr->next = new Node(otherCurr->value);
+
+            std::cout << curr->value << " ";
             curr = curr->next;
-            otherCurr = otherCurr->next;
+            std::cout << std::endl;
         }
     }
-
-    T& operator[] (int index)
+}
+template<typename T>
+inline Set<T>& Set<T>::operator=(const Set<T>& other)
+{
     {
-        Node* curr = head;
-        for (int i = 0; i < index && curr != nullptr; ++i)
-        {
-            curr = curr->next;
-        }
-        if (curr == nullptr)
-        {
-            std::ostringstream oss;
-            oss << "Error: Cannot get element at index #" << index << std::endl;
-            throw std::out_of_range(oss.str());
-        }
-        return curr->value;
-    }
-
-    const T& operator[] (int index) const
-    {
-        Node* curr = head;
-        for (int i = 0; i < index && curr != nullptr; ++i)
-        {
-            curr = curr->next;
-        }
-        if (curr == nullptr)
-        {
-            std::ostringstream oss;
-            oss << "Error: Cannot get element at index #" << index << std::endl;
-            throw std::out_of_range(oss.str());
-        }
-        return curr->value;
-    }
-
-    Set& operator=(const Set& other) {
         if (this == &other) {
             return *this;
         }
@@ -78,68 +109,57 @@ public:
         {
             return *this;
         }
-        head = new Node(other.head->data);
+        head = new Node(other.head->value);
         Node* curr = head;
         Node* otherCurr = other.head->next;
         while (otherCurr != nullptr)
         {
-            curr->next = new Node(otherCurr->data);
+            curr->next = new Node(otherCurr->value);
             curr = curr->next;
             otherCurr = otherCurr->next;
         }
         return *this;
     }
+}
 
-  /*  void add(T val)
+template<typename T>
+inline Set<T>::Set(const Set& other)
+    : head(nullptr)
+{
+    if (other.head == nullptr)
     {
-        if (head == nullptr)
-        {
-            head = new Node(val);
-            return;
-        }
-        Node* curr = head;
-        while (curr->next != nullptr)
-        {
-            curr = curr->next;
-        }
-        curr->next = new Node(val);
-    }*/
-
-    void printAll() const
-    {
-        Node* curr = head;
-        if (curr == nullptr)
-        {
-            std::cout << "List is empty :(\n";
-        }
-        else
-        {
-            while (curr != nullptr)
-            {
-
-                std::cout << curr->value << " ";
-                curr = curr->next;
-                std::cout << std::endl;
-            }
-        }
+        return;
     }
+    head = new Node(other.head->value);
+    Node* curr = head;
+    Node* otherCurr = other.head->next;
+    while (otherCurr != nullptr) {
+        curr->next = new Node(otherCurr->value);
+        curr = curr->next;
+        otherCurr = otherCurr->next;
+    }
+}
+template<typename T>
+inline Set<T>::Set(T* elems, int n)
+{
 
-    //void insert(VolShape* val, int index)
-    //{
-    //    Node* newNode = new Node(val);
-    //    Node phantom(new Cylinder, head);
-    //    Node* curr = &phantom;
-    //    for (int i = 0; i < index && curr != nullptr; ++i)
-    //    {
-    //        curr = curr->next;
-    //    }
-    //    if (curr == nullptr)
-    //    {
-    //        delete newNode;
-    //        throw std::out_of_range("Error: Cannot insert at specified position\n");
-    //    }
-    //    newNode->next = curr->next;
-    //    curr->next = newNode;
-    //    head = phantom.next;
-    //}
-};
+}
+
+template<typename T>
+inline Set<T>::Set(T x)
+{
+
+}
+
+template<typename T>
+inline Set<T>::~Set()
+{
+    Node* curr = head;
+    Node* temp;
+    while (curr != nullptr)
+    {   
+        temp = curr;
+        curr = curr->next;
+        delete temp;
+    }
+}
