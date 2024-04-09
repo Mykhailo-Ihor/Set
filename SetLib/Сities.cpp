3d93007e6647b5bccc07318ce3cc369515af1dfc
Михайло-Ігор Проць <MykhailoIhor.Prots@lnu.edu.ua>
2024-04-09 18:43:57 +0300

    Додано ітератор, прибрано метод get_head

--- SetLib/Сities.cpp
+++ SetLib/Сities.cpp
@@ -30,34 +30,34 @@ void find_route(const CitiesTable& table,cities start, cities end)
     else
     {
         bool flag = false;
-        Set<cities>::Node* curr = starting_city.get_head();
+        Set<cities>::Iterator curr = starting_city.begin();
         while (curr != nullptr)
         {
-            Set<cities> possible_route1 = table.routes[(int)curr->value];
+            Set<cities> possible_route1 = table.routes[(int)*curr];
             if (possible_route1.contain(end))
             {
                 std::cout << "–ейс в≥д м≥ста " << start << " до м≥ста " << end << std::endl;
-                std::cout << start << " -> " << curr->value << " -> " << end << std::endl;
+                std::cout << start << " -> " << *curr << " -> " << end << std::endl;
                 flag = true;
                 break;
             }
             else
             {
-                Set<cities>::Node* curr_possible_route = possible_route1.get_head();
+                Set<cities>::Iterator curr_possible_route = possible_route1.begin();
                 while (curr_possible_route != nullptr)
                 {
-                    Set<cities> possible_route2 = table.routes[(int)curr_possible_route->value];
+                    Set<cities> possible_route2 = table.routes[(int)*curr_possible_route];
                     if (possible_route2.contain(end))
                     {
                         std::cout << "–ейс в≥д м≥ста " << start << " до м≥ста " << end << std::endl;
-                        std::cout << start << " -> " << curr->value << " -> " << curr_possible_route->value << " -> " << end << std::endl;
+                        std::cout << start << " -> " << *curr << " -> " << *curr_possible_route << " -> " << end << std::endl;
                         flag = true;
                         break;
                     }
-                    curr_possible_route = curr_possible_route->next;
+                    ++curr_possible_route;
                 }
             }
-            curr = curr->next;
+            ++curr;
         }
         if (!flag)
         {
